0979f844aca910c6a3589c60e034a84d9ff40508
Dharsha-Gangadhar <darshan75462020@gmail.com>
2026-08-27 22:06:30 +0530

    first problem

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1=float(input("Enter a number"))
+num2=float(input("Enter a number"))
+ch=input("Enter the operator")[0]
+if(ch=='+'):
+    print(num1+num2)
+elif(ch=='-'):
+        print(num1-num2)
+elif(ch=='*'):
+            print(num1*num2)
+elif(ch=='/'):
+             print(num1/num2)
+else:
+     print("Enter correct values")
+                    
