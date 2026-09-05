d3f667f9f3353906e1fb4880bb2ca1b84b44c465
Dharsha-Gangadhar <darshan75462020@gmail.com>
2026-09-05 19:13:35 +0530

    day

---
+++ day1_basics.py
@@ -0,0 +1,19 @@
+# num1=float(input("Enter a number"))
+# num2=float(input("Enter a number"))
+# ch=input("Enter the operator")[0]
+# if(ch=='+'):
+#     print(num1+num2)
+# elif(ch=='-'):
+#         print(num1-num2)
+# elif(ch=='*'):
+#             print(num1*num2)
+# elif(ch=='/'):
+#              print(num1/num2)
+# else:
+#      print("Enter correct values")
+                    
+#-- info--#
+a=input("enter the name: ")
+b=int(input("enter the age: "))
+c=input("enter the city: ")
+print("Hello,",a," you are " ,b," years old and you live in ",c)
