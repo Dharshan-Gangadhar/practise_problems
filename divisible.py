38532afc1174b0922cb204b98d69168bf8fd6f6d
Dharsha-Gangadhar <darshan75462020@gmail.com>
2026-09-06 22:31:06 +0530

    Easy divisible

---
+++ divisible.py
@@ -0,0 +1,17 @@
+# a=int(input("enter a number: "))
+# if a%3==0 and a%5==0:
+#     print("Divisible by both 3 and 5")
+# else:
+#     print("Not divisible by both 3 and 5")
+# a=int(input("enter a number: "))
+# b=int(input("enter another number: "))
+# if a>b:
+#     print(b)
+# else:
+#     print(a)
+a=int(input("enter a number: "))
+if a%10==0:
+    print("multiple of 10")
+else:
+    print("not a multiple of 10")
+    
